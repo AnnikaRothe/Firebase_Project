e8454d1d4212d24f60487857e55d259dc81a8744
Programmika <rothe.annika@yahoo.com>
2023-07-20 17:38:40 +0200

    added delete function

---
+++ src/app/notes/notes.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Firestore, addDoc, collection, collectionData, doc, deleteDoc, } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent {
+
+  notes$!: Observable<any[]>;
+  noteCollection = collection(this.firestore, 'notes');
+
+  constructor(private firestore: Firestore) { 
+    this.getNotes();
+  }
+  
+  // Initialize the 'notiz' object to store the name and note entered by the user in the form
+  note = {
+    name: '',
+    note: ''
+  };
+
+  // Array to store the entered notes
+  notes: { name: string, note: string }[] = [];
+
+  // This method is called when the form is submitted
+  addNote() {
+    addDoc(this.noteCollection, this.note);
+}
+
+getNotes() {
+  this.notes$ = collectionData(this.noteCollection, { idField: 'id' });
+
+  this.notes$.subscribe((note) => {
+    console.log(note);
+  });
+}
+
+deleteNote(id: string) {
+  const userDoc = doc(this.noteCollection, id);
+  deleteDoc(userDoc);
+}
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'notizblock-ff5bc',
+    appId: '1:868050945835:web:f6ea2db71a7c0ecb75e76e',
+    storageBucket: 'notizblock-ff5bc.appspot.com',
+    apiKey: 'AIzaSyCyfOfaLSsT_TE-wrvAReq8yRkAtoyQ4Fs',
+    authDomain: 'notizblock-ff5bc.firebaseapp.com',
+    messagingSenderId: '868050945835',
+  },
+};
